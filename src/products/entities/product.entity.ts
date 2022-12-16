69caa0055c04de6b001c5b3048df0e51a4da1fe9
Giancarlo Culcay <giancode@outlook.com>
2022-12-16 13:35:44 -0500

    TypeORM - creacion: Product Entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,15 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// Recuerda el entity es una representacion de esta clase en la db, es como la tabla
+// * recuerda configuramos autoLoadEntities y synchronize en true, inmediatamente lo sincroniza con la db todo lo q estoy poniendo abajo:
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  // ! no todos los tipos de datos q me muestra, estan soportados por postgres, aca estan de todos: mysql, mongo, ...
+  @Column('text', {
+    unique: true,
+  })
+  title: string;
+}
