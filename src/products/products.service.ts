83209701f6027e32b99576bd63d4b6bfc3fd3171
Giancarlo Culcay <gieg5c@gmail.com>
2023-02-07 22:01:29 -0500

    Manejo de errores

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -7,6 +12,9 @@ import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  private readonly logger = new Logger('ProductsService'); // propiedad privada que no se modifica
+  // dentro de Logger(), nombre de la clase en la cual uso el logger
+
   // Patron Repositorio
   constructor(
     @InjectRepository(Product)
@@ -21,8 +29,11 @@ export class ProductsService {
 
       return product;
     } catch (error) {
-      console.log(error);
-      throw new InternalServerErrorException('!Ayuda');
+      // console.log(error);  // no se visualiza rapidamente el error
+
+      //this.logger.error(error); // mas fácil ver el error
+      // throw new InternalServerErrorException('!Ayuda');
+      this.handleDBExceptions(error);
     }
   }
 
@@ -40,5 +51,13 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleDBExceptions(error: any) {
+    if (error.code === '23505') throw new BadRequestException(error.detail);
+    this.logger.error(error);
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs',
+    );
   }
 }
