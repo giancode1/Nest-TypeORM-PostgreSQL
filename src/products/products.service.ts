e16574c997901fb9bbfdcf2e9b1c73b1e9d025d7
Giancarlo Culcay <gieg5c@gmail.com>
2023-02-09 14:23:34 -0500

    Buscar por Slug o UUID

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -11,6 +11,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService {
@@ -45,11 +46,19 @@ export class ProductsService {
     });
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id });
+  async findOne(term: string) {
+    // * Buscar por UUID o slug:
+    let product: Product;
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      product = await this.productRepository.findOneBy({ slug: term });
+    }
+
+    // const product = await this.productRepository.findOneBy({ id });
 
     if (!product)
-      throw new NotFoundException(`Product with id ${id} not found`);
+      throw new NotFoundException(`Product with id ${term} not found`);
 
     return product;
   }
